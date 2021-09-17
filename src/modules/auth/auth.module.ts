cb785b9e6ef1cc326353f11f2f65c73ef0ce4497
gerpan <nhaths4701@gmail.com>
2021-09-17 20:04:43 +0700

    Format code

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from '@modules/users/users.module';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
